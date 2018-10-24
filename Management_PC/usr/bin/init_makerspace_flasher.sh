23083f8cf2b27032cf3366474fc65f3a6697b1ef
jmrahm <julian.rahm@tu-dresden.de>
2018-10-24 16:58:27 +0200

    little naming fix

--- Management_PC/usr/bin/init_makerspace_flasher.sh
+++ Management_PC/usr/bin/init_makerspace_flasher.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-/home/makerspace/Downloads/MRT_RPi_Management_Scripts/Management_PC/usr/bin/update_raspbian
+chmod +x /home/makerspace/Downloads/MRT_RPi_Management_Scripts/Management_PC/usr/bin/update_raspbian.sh
+chmod +x /home/makerspace/Downloads/MRT_RPi_Management_Scripts/Management_PC/usr/bin/check_dev
+
+/home/makerspace/Downloads/MRT_RPi_Management_Scripts/Management_PC/usr/bin/update_raspbian.sh
 /home/makerspace/Downloads/MRT_RPi_Management_Scripts/Management_PC/usr/bin/check_dev $(find /home/makerspace/Downloads/*-raspbian-*-lite.img) /home/makerspace/Downloads/MRT_RPi_FirmwarePatch/PLT_RPi-Update.tgz
